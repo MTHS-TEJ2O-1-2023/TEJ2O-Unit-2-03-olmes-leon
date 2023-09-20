121a4b6efdce3018f238cc61f9df0cd58c68bdb8
olmes-leon <144264100+olmes-leon@users.noreply.github.com>
2023-09-20 13:03:05 -0400

    fixed reed me

--- main.ts
+++ main.ts
@@ -5,4 +5,19 @@
  * This program ...
 */
 
-basic.showString('Hello, World!')
+/*hint: a = l x w or a = 5x3 and p = 2(l + w) or p = 2(5 + 3)*/
+
+basic.showNumber(1 + 1)
+
+basic.showNumber(2 + 1)
+
+basic.showNumber(4 - 1)
+
+basic.showNumber(2 * 2)
+
+basic.showNumber(6 / 2)
+
+
+
+
+

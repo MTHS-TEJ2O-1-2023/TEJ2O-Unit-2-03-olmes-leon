8edfd2d52ca0ce90c198e8afa70e9780e34d889c
olmes-leon <144264100+olmes-leon@users.noreply.github.com>
2023-09-20 13:17:33 -0400

    blha

--- main.ts
+++ main.ts
@@ -7,17 +7,4 @@
 
 /*hint: a = l x w or a = 5x3 and p = 2(l + w) or p = 2(5 + 3)*/
 
-basic.showNumber(1 + 1)
-
-basic.showNumber(2 + 1)
-
-basic.showNumber(4 - 1)
-
-basic.showNumber(2 * 2)
-
-basic.showNumber(6 / 2)
-
-
-
-
 
